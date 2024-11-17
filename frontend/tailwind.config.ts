54ac4f3ddd861ebdd9ea23b6b9dfc411e6ecd0dc
idontwannapickaname <lightrageg@gmail.com>
2024-11-17 21:57:04 +0700

    initial create app

---
+++ frontend/tailwind.config.ts
@@ -0,0 +1,60 @@
+import type { Config } from "tailwindcss";
+import { nextui } from "@nextui-org/theme";
+
+const config: Config = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "auth-image": "url('/images/authimage.png')",
+      },
+      colors: {},
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      fontFamily: {
+        roboto: ["Roboto", "sans - serif"],
+        robotoslab: ["Roboto Slab", "serif"],
+        robotoflex: ["Roboto Flex", "sans - serif"],
+        inter: ["Inter", "sans - serif"],
+        itim: ["Itim", "cursive"],
+      },
+    },
+    screens: {
+      mobile: {
+        min: "100px",
+        max: "550px",
+      },
+      tablet: {
+        min: "550px",
+        max: "750px",
+      },
+      "mini-laptop": {
+        min: "750px",
+        max: "874px",
+      },
+      laptop: {
+        min: "874px",
+        max: "1280px",
+      },
+      desktop: {
+        min: "1280px",
+      },
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+    },
+  },
+  darkMode: "class",
+  plugins: [nextui()],
+};
+export default config;
