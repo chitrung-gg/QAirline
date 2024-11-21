d8087ae28ee5d4102804c82cb15e65ee3b8350b9
Trang Vuong <trangvuong2810@gmail.com>
2024-11-21 23:52:14 +0700

    add card components

---
+++ frontend/components/Card/FlightCard.tsx
@@ -0,0 +1,82 @@
+import {Card, CardHeader, CardBody, CardFooter, Divider, Link, Image, Button} from "@nextui-org/react";
+
+export default function FlightCard() {
+
+    const data = {
+        brand: "QAirline", //cái này hơi thừa, mình có brand nào khác đâu :v
+        departure_location: "HAN",
+        departure_time: "12:00",
+        departure_airport: "Sân bay Nội bài", //cái này cần check lại, 
+        //vì HAN hnhu là mã của sân bay nội bài r còn đâu, ghi thêm thì thừa??
+        arrival_location: "SGN",
+        arrival_time: "14:30",
+        arrival_airport: "Sân bay Tân Sơn Nhất",
+        logo: "/images/Qairline.png",
+        price: 500000,
+        duration: "2h 30m",
+        type: "Economy",
+    }
+
+  return (
+    <div className="gap-5 grid place-items-center" >
+        <Card className="w-4/5 ">
+            <CardHeader className="justify-between">
+                <div className="flex gap-3">
+                    <Image
+                        alt="logo"
+                        height={40}
+                        src={data.logo}
+                        width={40}
+                        className='transition-all hover:scale-100'
+                    />
+                    <div className="flex flex-col justify-center items-center">
+                        <p className="text-md">{data.brand}</p>
+                    </div>
+                </div>
+                <div className="flex flex-col mr-3">
+                    <p className="text-md">Hạng: {data.type}</p>
+                </div>
+            </CardHeader>
+            <Divider className="bg-transparent"/>
+            <CardBody>
+                <div className="flex">
+                    <div className="w-4/5 flex items-center justify-between mx-2">
+                        <div className="flex-1 flex flex-col items-start">
+                            <p className="text-lg font-bold">{data.departure_time}</p>
+                            <p>{data.departure_location}</p>
+                            <p>{data.departure_airport}</p>
+                        </div>
+                        <div className="mx-4 flex flex-col items-center">
+                            <p>-----------------------</p>
+                            <p>{data.duration}</p>
+                        </div>
+                        <div className="flex-1 flex flex-col items-end">
+                            <p className="text-lg font-bold">{data.arrival_time}</p>
+                            <p>{data.arrival_location}</p>
+                            <p>{data.arrival_airport}</p>
+                            
+                        </div>
+                    </div>
+                    <div className="w-1/5 flex items-center justify-between mx-2">
+                        <div className="flex-1 flex flex-col items-center">
+                            <p className="text-lg font-bold">{data.price.toLocaleString()} VNĐ</p>
+                            <Button className="mt-2 bg-blue-normal text-white">Đặt vé ngay</Button>
+                        </div>
+                    </div>
+                </div>
+            </CardBody>
+            <Divider className="bg-transparent"/>
+            <CardFooter className="justify-between">
+                <div className="flex gap-3">
+                    <div className="flex flex-col justify-center items-center">
+                        <p className="text-md ml-1">Có thể hoàn tiền một phần</p>
+                    </div>
+                </div>
+                <div className="flex flex-col mr-3">
+                    <Link href="#" className="bg-transparent text-blue-normal text-md font-semibold">Chi tiết hành trình</Link>
+                </div>
+            </CardFooter>
+        </Card>
+    </div>
+  );
+}
---
+++ frontend/components/Card/LocationCard.tsx
@@ -0,0 +1,33 @@
+"use client";
+import {Card, CardFooter, Image, Button, CardHeader} from "@nextui-org/react";
+
+export default function LocationCard() {
+    const data = {
+        title: "Hà Nội",
+        country: "Việt Nam",
+        img: "/images/Qairline.png",
+        description: "Thủ đô của Việt Nam",
+    }
+
+  return (
+    <div className="gap-5 grid" >
+        <Card isPressable onPress={() => console.log("item pressed")} className="w-full h-[300px] col-span-12 sm:col-span-7">
+            <Image
+                removeWrapper
+                alt="Location image"
+                className="z-0 w-full h-full object-cover"
+                src={data.img}
+            />
+            <CardFooter className="absolute bottom-3 z-10 ml-2">
+                <div className="flex flex-grow gap-2 items-center">
+                    <div className="flex flex-col text-left">
+                        <p className="font-bold text-2xl">{data.title}</p>
+                        <p className="text-lg">{data.country}</p>
+                        <p className="">{data.description}</p>
+                    </div>
+                </div>
+            </CardFooter>
+        </Card>
+    </div>
+  );
+}
